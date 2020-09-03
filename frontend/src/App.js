17a613f048ce6ef3daef38418d5e71725466bd9d
Leonardo81 <leonardoeinstein2000@gmail.com>
2020-09-03 15:41:50 +0700

    apply persisted reducer on buat jadwal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Provider } from "react-redux";
 import { Router } from "react-router";
 import { createBrowserHistory } from "history";
+import { PersistGate } from "redux-persist/integration/react";
 
 import MediaQuery from "containers/MediaQuery";
 import Loading from "containers/Loading";
 
 import Routes from "./routes";
-import store from "./redux/store";
+import store, { persistor } from "./redux/store";
 
 import config from "config";
 import "./app.css";
@@ -18,9 +19,11 @@ function App() {
   return (
     <Router history={history}>
       <Provider store={store}>
-        <Routes />
-        <MediaQuery />
-        <Loading />
+        <PersistGate loading={null} persistor={persistor}>
+          <Routes />
+          <MediaQuery />
+          <Loading />
+        </PersistGate>
       </Provider>
     </Router>
   );
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,4 +1,16 @@
 import { createStore } from "redux";
 import rootReducer from "./reducers";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from 'redux-persist';
 
-export default createStore(rootReducer);
+const persistConfig = {
+  key: 'jadwal',
+  storage: storage,
+  whitelist: ['schedules', 'courses']
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer);
+
+export const persistor = persistStore(store);
+export default store;
