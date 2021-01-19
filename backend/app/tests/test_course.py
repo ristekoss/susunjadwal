535a84bea927795ea4ffbca800614ffe096321c4
Reyhan Alhafizal <reyhanhamidi@gmail.com>
2021-01-19 07:29:30 +0700

    Create test for courses related views

---
+++ backend/app/tests/test_course.py
@@ -0,0 +1,69 @@
+from app import app
+from models.period import Class, Course, Period, ScheduleItem
+from .utils import BASE_PATH
+
+
+class TestCourse:
+    """Test get course list endpoint
+    This test class use auth_client fixture. See conftest.py for more info."""
+
+    COURSE = {
+        'name': 'Analisis Numerik',
+        'credit': 3,
+        'term': 6,
+    }
+
+    def test_get_courses(self, auth_client):
+        client, user = auth_client
+        self.create_period(user.major.id)
+
+        url = '{}/majors/{}/courses'.format(BASE_PATH, user.major.id)
+        res = client.get(url)
+
+        assert res.status_code == 200
+        res_json = res.get_json()
+        assert res_json['is_detail']
+        assert res_json['name'] == app.config["ACTIVE_PERIOD"]
+
+        assert len(res_json['courses']) == 1
+        course_data = res_json['courses'][0]
+        assert course_data['name'] == self.COURSE['name']
+        assert course_data['credit'] == self.COURSE['credit']
+        assert course_data['term'] == self.COURSE['term']
+
+        assert len(course_data['classes']) == 1
+        class_data = course_data['classes'][0]
+        assert len(class_data['lecturer']) == 2
+        assert len(class_data['schedule_items']) == 1
+
+    def create_period(self, major_id):
+        """Create dummy period with its course and class"""
+
+        class_obj = Class(
+            name='Anum - A',
+            schedule_items=[
+                ScheduleItem(
+                    day='Senin',
+                    start='09.40',
+                    end='08.00',
+                    room='A6.09 (Ged Baru)',
+                ),
+            ],
+            lecturer=['Nama Dosen 1', 'Nama Dosen 2'],
+        )
+
+        course = Course(
+            name=self.COURSE['name'],
+            credit=self.COURSE['credit'],
+            term=self.COURSE['term'],
+            classes=[class_obj],
+        )
+
+        period = Period.objects().create(
+            major_id=major_id,
+            name=app.config["ACTIVE_PERIOD"],
+            is_detail=True,
+            courses=[course],
+        )
+
+        return period
